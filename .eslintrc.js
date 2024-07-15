5b04dd80672e9aac410dddfb028b77ba5e09ed67
Changmin Kim <ckd29672@naver.com>
2024-07-16 00:53:02 +0900

    chore: 자동 포맷팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,39 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    'extends': [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "prettier"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    '@typescript-eslint/semi': ['error', 'always'],
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, semi: true, endOfLine: 'auto' },
     ],
-    'overrides': [
-        {
-            'env': {
-                'node': true
-            },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react",
-        "prettier"
-    ],
-    "rules": {
-        "@typescript-eslint/semi": ["error","always"],
-        "prettier/prettier": ["error", { "singleQuote": true, "semi": true, "endOfLine": "auto"}],
-        "space-before-function-paren": ["error", "never"],
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/explicit-module-boundary-types": ["error"] //함수 타입 지정
-    }
+    'space-before-function-paren': ['error', 'never'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': ['error'], //함수 타입 지정
+  },
 };
