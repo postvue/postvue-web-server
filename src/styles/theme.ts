7e3034a6c16d2a989ecc73e0ed0d9f519aa3f7e9
Changmin Kim <ckd29672@naver.com>
2024-07-30 10:12:49 +0900

    style: css 스타일 설정
    - grey 스타일 수정
    - 메인 칼러 추가: Blue
    - fontsize: 폰트 일부 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,21 +1,22 @@
 const mainColor = {
   SkyBlue0: '#88C7F5',
   SkyBlue1: '#54A1D9',
+  Blue: '#1D77FF',
   White: '#FFFFFF',
   Black: '#000000',
 };
 
 const grey = {
   Grey0: '#F8F9FA',
-  Grey1: '#F1F3F5',
-  Grey2: '#EAECEF',
-  Grey3: '#DEE2E6',
-  Grey4: '#CED4DA',
-  Grey5: '#ACB5BD',
-  Grey6: '#858E96',
-  Grey7: '#495057',
-  Grey8: '#343A40',
-  Grey9: '#212529',
+  Grey1: '#F2F3F4',
+  Grey2: '#E8EBED',
+  Grey3: '#C8CDD2',
+  Grey4: '#ACB2B9',
+  Grey5: '#9199A1',
+  Grey6: '#787F87',
+  Grey7: '#535B63',
+  Grey8: '#3D4248',
+  Grey9: '#26292C',
 };
 
 const successColor = {
@@ -38,7 +39,7 @@ const fontSizes = {
   Subhead4: '500 22px/1.4 Pretendard-Bold; letter-spacing: -0.6px;',
   Subhead3: '500 20px/1.4 Pretendard-Bold; letter-spacing: -0.6px;',
   Subhead2:
-    '500 18px/1.4 Pretendard-Bold; letter-spacing: -0.6px; font-style: normal',
+    '600 18px/1.4 Pretendard; letter-spacing: -0.6px; font-style: normal',
   Subhead1: '500 16px/1.4 Pretendard-Bold; letter-spacing: -0.6px;',
   Body4: '500 20px/1.4 Pretendard-Medium',
   Body3:
@@ -49,6 +50,8 @@ const fontSizes = {
     '500 14px/1.4 Pretendard-Medium; letter-spacing: -0.6px; line-height: 140%;',
   Label:
     '300 14px/1.4 Pretendard-Medium; letter-spacing: -0.6px; line-height: 140%;',
+  Label1:
+    '400 14px/1.4 Pretendard-Medium; letter-spacing: -0.6px; line-height: 140%;',
   Element1:
     '300 14px/1.4 Pretendard-Regular; letter-spacing: -0.6px; line-height: 140%;',
 };
