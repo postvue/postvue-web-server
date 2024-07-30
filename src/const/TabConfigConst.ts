7be61ae7294ea58d9769fe09e431b4c528332cf9
Changmin Kim <ckd29672@naver.com>
2024-07-30 10:26:27 +0900

    refactor: tab 관련 상수 값 이름 수정

--- src/const/TabConfigConst.ts
+++ src/const/TabConfigConst.ts
@@ -1,5 +1,5 @@
-export const TasteForMeTabName = '추천';
-export const TasteForMeTabId = 1;
+export const TASTE_FOR_ME_TAB_NAME = '추천';
+export const TASTE_FOR_ME_TAB_ID = 1;
 
-export const FollowForMeTabName = '팔로잉';
-export const FollowForMeTabId = 2;
+export const FOLLOW_FOR_ME_TAB_NAME = '팔로잉';
+export const FOLLOW_FOR_ME_TAB_ID = 2;
