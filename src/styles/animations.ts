603db463ccf24e137350d2029115c3607e284278
Changmin Kim <ckd29672@naver.com>
2024-08-03 15:37:24 +0900

    style: styled-component 애니메이션 기능 설정
    - slideUp: 아래에서 위로 올라가는 모션
    - slideDown: 위에서 아래로 올라가는 모션

---
+++ src/styles/animations.ts
@@ -0,0 +1,28 @@
+import { keyframes } from 'styled-components';
+
+const slideUp = keyframes`
+  from {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+
+const slideDown = keyframes`
+  from {
+    transform: translateY(-20%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+
+export const animationStyle = {
+  slideUp,
+  slideDown,
+};
