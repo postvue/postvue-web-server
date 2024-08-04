c20aa0862c11d786595f5b814cd3f14e6e982a16
Changmin Kim <ckd29672@naver.com>
2024-08-04 21:05:13 +0900

    fix: 쿼리 파싱 오류 고침
    - 문제: 단어\ 검색 안되는 문제
    - qs 라이브러리로 해결

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import QueryString from 'qs';
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
@@ -7,4 +8,7 @@ export const api = axios.create({
     Accept: 'application/json',
   },
   withCredentials: true,
+  paramsSerializer: (params) => {
+    return QueryString.stringify(params, { encode: true });
+  },
 });
