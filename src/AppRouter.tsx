23bdccf5dadeb8548defc6c9c0284dfadc32bb06
Changmin Kim <ckd29672@naver.com>
2024-08-05 12:15:16 +0900

    chore: 검색 페이지 라우팅 설정

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { HOME_PATH, PROFILE_POST_LIST_PATH } from './const/PathConst';
+import {
+  HOME_PATH,
+  PROFILE_POST_LIST_PATH,
+  SEARCH_PATH,
+  SEARCH_POST_PATH,
+} from './const/PathConst';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
+import SearchPage from './pages/SearchPage';
+import SearchPostPage from './pages/SearchPostPage';
 
 const AppRouter: React.FC = (): JSX.Element => {
   return (
@@ -10,6 +17,8 @@ const AppRouter: React.FC = (): JSX.Element => {
       <Routes>
         <Route path={HOME_PATH} element={<HomePage />} />
         <Route path={PROFILE_POST_LIST_PATH} element={<ProfilePage />} />
+        <Route path={SEARCH_PATH} element={<SearchPage />} />
+        <Route path={SEARCH_POST_PATH} element={<SearchPostPage />} />
       </Routes>
     </BrowserRouter>
   );
