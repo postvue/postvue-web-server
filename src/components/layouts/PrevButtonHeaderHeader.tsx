0b133e71dd1d30784b9b04d95192778bd7f959cd
Changmin Kim <ckd29672@naver.com>
2024-08-14 22:01:54 +0900

    feat: 이전 버튼 포함된 헤더 레이아웃 개발

---
+++ src/components/layouts/PrevButtonHeaderHeader.tsx
@@ -0,0 +1,53 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+import theme from '../../styles/theme';
+import PrevButton from '../PrevButton';
+import HeaderLayout from './HeaderLayout';
+
+interface PrevButtonHeaderProps {
+  titleName: string;
+  RightButtonNode?: ReactNode;
+}
+
+const PrevButtonHeaderHeader: React.FC<PrevButtonHeaderProps> = ({
+  titleName,
+  RightButtonNode,
+}) => {
+  return (
+    <HeaderLayout>
+      <PrevButtonHeaderContainer>
+        <PrevButtonHeaderHeaderWrap>
+          <PrevButtonWrap>
+            <PrevButton strokeColor={theme.mainColor.Black} />
+          </PrevButtonWrap>
+          <TitleName>{titleName}</TitleName>
+          {RightButtonNode}
+        </PrevButtonHeaderHeaderWrap>
+      </PrevButtonHeaderContainer>
+    </HeaderLayout>
+  );
+};
+
+const PrevButtonHeaderContainer = styled.div`
+  margin: auto 0;
+  width: 100%;
+  padding: 0 22px 0 15px;
+`;
+
+const PrevButtonHeaderHeaderWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PrevButtonWrap = styled.div`
+  display: flex;
+`;
+
+const TitleName = styled.div`
+  font: ${({ theme }) => theme.fontSizes.Subhead3};
+  position: fixed;
+  transform: translate(-50%, 0px);
+  left: 50%;
+`;
+
+export default PrevButtonHeaderHeader;
