585e4c16add69aef4eb2d60e0d6216f6ab438a98
Changmin Kim <ckd29672@naver.com>
2024-08-14 22:01:38 +0900

    feat: 헤더 레이아웃 개발

---
+++ src/components/layouts/HeaderLayout.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+import theme from '../../styles/theme';
+
+interface HeaderLayoutProps {
+  children: ReactNode;
+}
+
+const HeaderLayout: React.FC<HeaderLayoutProps> = ({ children }) => {
+  return <HeaderLayoutContainer>{children}</HeaderLayoutContainer>;
+};
+
+const HeaderLayoutContainer = styled.div`
+  height: ${theme.systemSize.header.height};
+  display: flex;
+  width: 100%;
+`;
+
+export default HeaderLayout;
