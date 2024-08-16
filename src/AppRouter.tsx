a2be45d98ff84b671727f97a60bfccdf960019fb
Changmin Kim <ckd29672@naver.com>
2024-08-16 14:34:06 +0900

    feat: 페이지 라우팅 설정

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,13 +4,23 @@ import {
   HOME_PATH,
   MESSAGE_CONVERSTAION_PATH,
   MESSAGE_INBOX_PATH,
+  PROFILE_ACCOUNT_PATH,
+  PROFILE_MY_CLIP_LIST_PATH,
+  PROFILE_MY_SCRAP_LIST_PATH,
+  PROFILE_MY_SCRAP_PATH,
+  PROFILE_NEW_SCRAP_PATH,
   PROFILE_POST_LIST_PATH,
   SEARCH_PATH,
   SEARCH_POST_PATH,
 } from './const/PathConst';
 import HomePage from './pages/HomePage';
+import MakeScrapPage from './pages/MakeScrapPage';
 import MessageConversationPage from './pages/MessageConversationPage';
 import MessageInboxPage from './pages/MessageInboxPage';
+import MyProfileClipPage from './pages/MyProfileClipListPage';
+import MyProfileScrap from './pages/MyProfileScrap';
+import MyProfileScrapListPage from './pages/MyProfileScrapListPage';
+import ProfileAccountPage from './pages/ProfileAccountPage';
 import ProfilePostPage from './pages/ProfilePostPage';
 import SearchPage from './pages/SearchPage';
 import SearchPostPage from './pages/SearchPostPage';
@@ -28,6 +38,21 @@ const AppRouter: React.FC = (): JSX.Element => {
           path={MESSAGE_CONVERSTAION_PATH}
           element={<MessageConversationPage />}
         />
+        <Route
+          path={MESSAGE_CONVERSTAION_PATH}
+          element={<MessageConversationPage />}
+        />
+        <Route
+          path={PROFILE_MY_CLIP_LIST_PATH}
+          element={<MyProfileClipPage />}
+        />
+        <Route
+          path={PROFILE_MY_SCRAP_LIST_PATH}
+          element={<MyProfileScrapListPage />}
+        />
+        <Route path={PROFILE_MY_SCRAP_PATH} element={<MyProfileScrap />} />
+        <Route path={PROFILE_NEW_SCRAP_PATH} element={<MakeScrapPage />} />
+        <Route path={PROFILE_ACCOUNT_PATH} element={<ProfileAccountPage />} />
       </Routes>
     </BrowserRouter>
   );
