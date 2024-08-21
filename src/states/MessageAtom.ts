1612a05e913cde274201a0dd799f030da176d20b
Changmin Kim <ckd29672@naver.com>
2024-08-21 16:04:14 +0900

    feat: Websocket 연결 모듈화

--- src/states/MessageAtom.ts
+++ src/states/MessageAtom.ts
@@ -22,3 +22,40 @@ export const cursorIdAtomByMsgConversation = atom<string>({
   key: 'cursorIdAtomByMsgConversation',
   default: INIT_CURSOR_ID,
 });
+
+export const msgBlockUserHashMapAtom = atom<Map<string, MsgBlockHiddenUser>>({
+  key: 'msgBlockUserHashMap',
+  default: new Map(),
+});
+
+export const cursorIdByMsgBlockUserAtom = atom<string>({
+  key: 'cursorIdByMsgBlockUserAtom',
+  default: INIT_CURSOR_ID,
+});
+
+export const msgHiddenUserHashMapAtom = atom<Map<string, MsgBlockHiddenUser>>({
+  key: 'msgHiddenUserHashMap',
+  default: new Map(),
+});
+
+export const cursorIdByMsgHiddenUserAtom = atom<string>({
+  key: 'cursorIdByMsgHiddenUserAtom',
+  default: INIT_CURSOR_ID,
+});
+
+export const isSettingByMsgConversationAtom = atom<boolean>({
+  key: 'isSettingByMsgConversation',
+  default: false,
+});
+
+export const msgReactionInfoAtom = atom<MsgReactionInfo>({
+  key: 'msgReactionInfo',
+  default: {
+    msgId: '',
+    msgHeight: 0,
+    y: 0,
+    height: 0,
+    isMyMsg: false,
+    msgText: '',
+  },
+});
