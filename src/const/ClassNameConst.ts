5db1104fa3aa577d2cb19f86d0f228cea39607bb
Changmin Kim <ckd29672@naver.com>
2024-08-21 16:05:57 +0900

    feat: 메시지 관련 상수 값 추가

--- src/const/ClassNameConst.ts
+++ src/const/ClassNameConst.ts
@@ -1,2 +1,3 @@
 export const TABBAR_NAV_CLASS_NAME = 'tab-bar-href';
 export const ACTIVE_CLASS_NAME = 'active';
+export const MSG_REACTION_CLASS_NAME = 'msg-reaction-';
--- src/const/PageConfigConst.ts
+++ src/const/PageConfigConst.ts
@@ -1,2 +1,3 @@
 export const PAGE_NUM = 0;
 export const INIT_CURSOR_ID = '100000000000000000';
+export const ZERO_CURSOR_ID = '0';
