51f9a57f50236f16bb14ae1d79ad19749c629efc
Changmin Kim <ckd29672@naver.com>
2024-08-26 14:39:06 +0900

    fix: 스크랩 리스트 모듈로 변경

--- src/components/profile/ProfileScrapListBody.tsx
+++ src/components/profile/ProfileScrapListBody.tsx
@@ -6,9 +6,9 @@ import {
   PROFILE_MY_SCRAP_LIST_PATH,
   PROFILE_NEW_SCRAP_PATH,
 } from '../../const/PathConst';
-import ProfileScrapListInfiniteScroll from '../../hook/ProfileScrapListInfiniteScroll';
 import { myProfileScrapListAtom } from '../../states/ProfileAtom';
 import theme from '../../styles/theme';
+import ProfileScrapViewBody from '../common/body/ProfileScrapViewBody';
 
 const ProfileScrapListBody: React.FC = () => {
   const myProfileScrapList = useRecoilValue(myProfileScrapListAtom);
@@ -55,26 +55,13 @@ const ProfileScrapListBody: React.FC = () => {
 
   return (
     <>
-      <MyProfileScrapListsContainer ref={MakeScrapScrollRef}>
-        {myProfileScrapList.map((v, i) => (
-          <MyProfileScrapWrap key={i}>
-            <MyProfileScrapImgListWrap>
-              {v.postImagePathList.map((image, k) => (
-                <MyProfileScrapImgWrap
-                  key={k}
-                  onClick={() => {
-                    navigate(`${PROFILE_MY_SCRAP_LIST_PATH}/${v.scrapId}`);
-                  }}
-                >
-                  <MyProfileScrapImg src={image} />
-                </MyProfileScrapImgWrap>
-              ))}
-            </MyProfileScrapImgListWrap>
-            <MyProfileScrapListName>{v.scrapName}</MyProfileScrapListName>
-          </MyProfileScrapWrap>
-        ))}
-        <ProfileScrapListInfiniteScroll />
-      </MyProfileScrapListsContainer>
+      <ProfileScrapViewBody
+        profileScrapViewRef={MakeScrapScrollRef}
+        profileScrapList={myProfileScrapList}
+        onButtonEvent={(scrapId: string) => {
+          navigate(`${PROFILE_MY_SCRAP_LIST_PATH}/${scrapId}`);
+        }}
+      />
       <MakeScrapButtonWrap opacity={scrollOpacity}>
         <Link to={PROFILE_NEW_SCRAP_PATH}>
           <MakeScrapButton>
@@ -101,59 +88,6 @@ const ProfileScrapListBody: React.FC = () => {
     </>
   );
 };
-
-const MyProfileScrapListsContainer = styled.div`
-  height: calc(100vh - 65px - ${theme.systemSize.bottomNavBar.height});
-  overflow: scroll;
-  & {
-    -ms-overflow-style: none;
-    scrollbar-width: none;
-  }
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-const MyProfileScrapWrap = styled.div`
-  padding: 0 20px 12px 20px;
-`;
-
-const MyProfileScrapListName = styled.div`
-  padding-top: 7px;
-  font: ${({ theme }) => theme.fontSizes.Subhead3};
-`;
-
-const MyProfileScrapImgListWrap = styled.div`
-  display: flex;
-  gap: 3px;
-  overflow-x: auto;
-  white-space: nowrap;
-
-  & {
-    -ms-overflow-style: none;
-    scrollbar-width: none;
-  }
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-const MyProfileScrapImgWrap = styled.div`
-  width: 30%;
-  flex: 0 0 auto;
-  &:hover {
-    filter: brightness(0.7);
-  }
-  cursor: pointer;
-`;
-
-const MyProfileScrapImg = styled.div<{ src: string }>`
-  width: 100%;
-  vertical-align: bottom;
-  aspect-ratio: 3/4;
-  background: url(${(props) => props.src}) center center / cover;
-  border-radius: 8px;
-`;
 
 const MakeScrapButtonWrap = styled.div<{ opacity: number }>`
   z-index: 1000;
