a2210c410202cc2dd6ec37d19a3824603bb897f5
Changmin Kim <ckd29672@naver.com>
2024-08-28 13:42:25 +0900

    fix: 타이틀 수정: 클립 복사 -> 게시물 복사

--- src/const/SystemPhraseConst.ts
+++ src/const/SystemPhraseConst.ts
@@ -1,5 +1,5 @@
 export const NOT_DATE_TIME = '날짜를 표기 할 수 없습니다.';
-export const PROFILE_URL_CLIP_BOARD_TEXT = '클립 복사';
+export const PROFILE_URL_CLIP_BOARD_TEXT = '게시물 복사';
 export const FOLLOW_BUTTON_TEXT = '팔로우';
 export const SEARCH_INPUT_PHARSE_TEXT = '관심 있는 태그를 검색해보세요.';
 export const MESSAGE_SEARCH_INPUT_PHARSE_TEXT = '메시지 검색';
