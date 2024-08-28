d663dee7a22e7e10a15b95041af346c66945b240
Changmin Kim <ckd29672@naver.com>
2024-08-28 13:52:36 +0900

    feat: postion css 관련 상수 값 추가

--- src/const/AttributeConst.ts
+++ src/const/AttributeConst.ts
@@ -2,3 +2,5 @@ export const INIT_SCROLL_POSITION = 0;
 export const INIT_EMPTY_STRING_VALUE = '';
 export const OVERFLOW_HIDDEN = 'hidden';
 export const OVERFLOW_SCROLL = 'scroll';
+export const POSITION_FIXED = 'fixed';
+export const POSITION_STATIC = 'static';
