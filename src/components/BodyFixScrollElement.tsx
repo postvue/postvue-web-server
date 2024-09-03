43dd9fd1c75fbd12b3260935f246fa683f884722
Changmin Kim <ckd29672@naver.com>
2024-09-03 18:57:52 +0900

    fix: body 고정화 모듈 effect 수정

--- src/components/BodyFixScrollElement.tsx
+++ src/components/BodyFixScrollElement.tsx
@@ -1,12 +1,27 @@
-import React, { useEffect } from 'react';
-import { OVERFLOW_HIDDEN, OVERFLOW_SCROLL } from '../const/AttributeConst';
+import React, { useEffect, useRef } from 'react';
+
 const BodyFixScrollElement: React.FC = () => {
+  const scrollY = useRef<number>(0);
   useEffect(() => {
+    scrollY.current = window.scrollY;
     const bodyElement = document.body;
-    bodyElement.style.overflow = OVERFLOW_HIDDEN;
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollY.current}px`;
+    document.body.style.left = '0px';
+    document.body.style.right = '0px';
+    document.body.style.width = '100%'; // 레이아웃 흔들림 방지
+
+    // bodyElement.style.top = '0';
+    // bodyElement.style.left = '0';
 
     return () => {
-      bodyElement.style.overflow = OVERFLOW_SCROLL;
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.left = '';
+      document.body.style.right = '';
+      document.body.style.width = '';
+
+      window.scrollTo(0, scrollY.current);
     };
   }, []);
   return <></>;
