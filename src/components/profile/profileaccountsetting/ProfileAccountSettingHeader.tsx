589832f6fd78733d89eb4800a1f04e949d489d78
Changmin Kim <ckd29672@naver.com>
2024-09-14 12:11:30 +0900

    feat: 계절 설정 페이지 개발

---
+++ src/components/profile/profileaccountsetting/ProfileAccountSettingHeader.tsx
@@ -0,0 +1,8 @@
+import PrevButtonHeaderHeader from 'components/layouts/PrevButtonHeaderHeader';
+import React from 'react';
+
+const ProfileAccountSettingHeader: React.FC = () => {
+  return <PrevButtonHeaderHeader titleName="환경설정" />;
+};
+
+export default ProfileAccountSettingHeader;
---
+++ src/pages/ProfileAccountSettingPage.tsx
@@ -0,0 +1,15 @@
+import AppBaseTemplate from 'components/layouts/AppBaseTemplate';
+import ProfileAccountSettingBody from 'components/profile/profileaccountsetting/ProfileAccountSettingBody';
+import ProfileAccountSettingHeader from 'components/profile/profileaccountsetting/ProfileAccountSettingHeader';
+import React from 'react';
+
+const ProfileAccountSettingPage: React.FC = () => {
+  return (
+    <AppBaseTemplate>
+      <ProfileAccountSettingHeader />
+      <ProfileAccountSettingBody />
+    </AppBaseTemplate>
+  );
+};
+
+export default ProfileAccountSettingPage;
