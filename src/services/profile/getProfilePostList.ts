fbf6deab74e8c448a3ad0227649ab165b26bf515
Changmin Kim <ckd29672@naver.com>
2024-09-14 11:46:56 +0900

    fix: profile 포스트 무한스크롤, react query 화

--- src/services/profile/getProfilePostList.ts
+++ src/services/profile/getProfilePostList.ts
@@ -3,16 +3,19 @@ import { PostRsp } from '../../global/interface/post';
 import { POST_LIST_PATH, PROFILE_LIST_PATH } from '../appApiPath';
 import { CURSOR_PARAM } from '../appApiQueryParam';
 
-interface GetProfilePostListRsp {
+export interface GetProfilePostListRsp {
   cursorId: string;
   snsPostRspList: PostRsp[];
 }
 
 export const getProfilePostListByCursor = (
+  username: string,
   cursor: string,
 ): Promise<GetProfilePostListRsp> => {
   return api
-    .get(`${PROFILE_LIST_PATH}${POST_LIST_PATH}?${CURSOR_PARAM}=${cursor}`)
+    .get(
+      `${PROFILE_LIST_PATH}/${username}${POST_LIST_PATH}?${CURSOR_PARAM}=${cursor}`,
+    )
     .then((res) => {
       console.log(res.data);
       return res.data.data;
