00bf38ee8c4ca7c724d5229ac357b2772fb6ec27
Changmin Kim <ckd29672@naver.com>
2024-09-14 12:13:00 +0900

    fix: 바디 fix 모듈 제거

--- src/pages/MyProfileClipListPage.tsx
+++ src/pages/MyProfileClipListPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import BodyFixScrollElement from '../components/BodyFixScrollElement';
 import BottomNavBar from '../components/BottomNavBar';
 import AppBaseTemplate from '../components/layouts/AppBaseTemplate';
 import ProfileClipListBody from '../components/profile/ProfileClipListBody';
@@ -7,7 +6,6 @@ import ProfileClipScrapHeader from '../components/profile/ProfileClipScrapHeader
 const MyProfileClipPage: React.FC = () => {
   return (
     <AppBaseTemplate>
-      <BodyFixScrollElement />
       <ProfileClipScrapHeader />
       <ProfileClipListBody />
       <BottomNavBar />
