e695725725b849747df788445c1a8cc84bd5c172
Changmin Kim <ckd29672@naver.com>
2024-09-25 12:01:36 +0900

    feat: 콘텐츠 링크로 가져온 url로 게시글 정보

---
+++ src/services/post/createPostByResourceLink.ts
@@ -0,0 +1,21 @@
+import { PostComposeUploadByResourceLinkReq } from 'global/interface/post';
+import { POST_LIST_PATH, POST_UPLOAD_RESOURCE_LINK } from 'services/appApiPath';
+import { api } from '..';
+
+//@REFER: 해당 게시글 링크로만 저장 할 지 아니면 url로 저장 할 지 고민 필요
+export const createPostByResourceLink = (
+  postComposeUploadByResourceLinkReq: PostComposeUploadByResourceLinkReq,
+): Promise<boolean> => {
+  return api
+    .post(
+      `${POST_LIST_PATH}${POST_UPLOAD_RESOURCE_LINK}`,
+      postComposeUploadByResourceLinkReq,
+    )
+    .then((res) => {
+      console.log(res.data);
+      return res.data.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
