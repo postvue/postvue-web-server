b6e2f6133d0a09ca651d6eaecdc425325c92f4bf
Changmin Kim <ckd29672@naver.com>
2024-10-10 12:58:01 +0900

    feat: 각 페이지 routing 연동

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,29 +1,53 @@
-import MyProfileEditPage from 'pages/MyProfileEditPage';
+import LoginPage from 'pages/LoginPage';
+import NotificatinoPage from 'pages/NotificationPage';
 import PostComposePage from 'pages/PostComposePage';
+import ProfileAccountPasswordEditPage from 'pages/ProfileAccounPasswordEditPage';
+import ProfileAccountBirthdateEditPage from 'pages/ProfileAccountBirthdateEditPage';
+import ProfileAccountDeleteAccountPage from 'pages/ProfileAccountDeleteAccountPage';
+import ProfileAccountEmailEditPage from 'pages/ProfileAccountEmailEditPage';
+import ProfileAccountGenderEditPage from 'pages/ProfileAccountGenderEditPage';
+import ProfileAccountPrivateProfilePage from 'pages/ProfileAccountPrivateProfilePage';
+import ProfileAccountSettingEditPage from 'pages/ProfileAccountSettingEditPage';
+import ProfileAccountSettingManagePage from 'pages/ProfileAccountSettingManagePage';
 import ProfileAccountSettingPage from 'pages/ProfileAccountSettingPage';
+import ProfileAccountSettingPrivacyPage from 'pages/ProfileAccountSettingPrivacyPage';
+import ProfileBlockedUserListManagePage from 'pages/ProfileBlockedUserListManagePage';
 import ProfileFollowListPage from 'pages/ProfileFolllowListPage';
 import SearchFavoriteTermEditPage from 'pages/SearchFavoriteTermEditPage';
+import SignupPage from 'pages/SignupPage';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {
   HOME_PATH,
+  LOGIN_PATH,
   MESSAGE_CONVERSTAION_PATH,
   MESSAGE_INBOX_PATH,
   MSG_BLOCK_LIST_MANAGE_PATH,
   MSG_HIDDEN_LIST_MANAGE_PATH,
+  NOTIFICATION_LIST_PATH,
   POST_COMPOSE_PATH,
   PROFILE_ACCOUNT_PATH,
+  PROFILE_BIRTHDATE_EDIT_PATH,
+  PROFILE_BLOCKED_ACCOUNT_PATH,
   PROFILE_CLIP_LIST_PATH,
+  PROFILE_DELETE_ACCOUNT_PATH,
   PROFILE_EDIT_PATH,
+  PROFILE_EMAIL_EDIT_PATH,
   PROFILE_FOLLOW_LIST_PATH,
+  PROFILE_GENDER_EDIT_PATH,
+  PROFILE_MANAGE_PATH,
   PROFILE_NEW_SCRAP_PATH,
+  PROFILE_PASSWORD_EDIT_PATH,
   PROFILE_POST_LIST_PATH,
+  PROFILE_PRIVACY_POLICY_PATH,
+  PROFILE_PRIVATE_PROFILE_PATH,
   PROFILE_SCRAP_LIST_PATH,
   PROFILE_SCRAP_PATH,
   PROFILE_SETTING_PATH,
   SEARCH_FAVORITE_LIST_PATH,
   SEARCH_PATH,
   SEARCH_POST_PATH,
+  SIGNUP_PATH,
 } from './const/PathConst';
 import HomePage from './pages/HomePage';
 import MakeScrapPage from './pages/MakeScrapPage';
@@ -52,10 +76,6 @@ const AppRouter: React.FC = (): JSX.Element => {
           path={MESSAGE_CONVERSTAION_PATH}
           element={<MessageConversationPage />}
         />
-        <Route
-          path={MESSAGE_CONVERSTAION_PATH}
-          element={<MessageConversationPage />}
-        />
         <Route path={PROFILE_CLIP_LIST_PATH} element={<MyProfileClipPage />} />
         <Route
           path={PROFILE_SCRAP_LIST_PATH}
@@ -76,17 +96,64 @@ const AppRouter: React.FC = (): JSX.Element => {
           path={PROFILE_FOLLOW_LIST_PATH}
           element={<ProfileFollowListPage />}
         />
-
-        <Route path={PROFILE_EDIT_PATH} element={<MyProfileEditPage />} />
         <Route
           path={PROFILE_SETTING_PATH}
           element={<ProfileAccountSettingPage />}
+        />
+        <Route
+          path={PROFILE_EDIT_PATH}
+          element={<ProfileAccountSettingEditPage />}
+        />
+        <Route
+          path={PROFILE_MANAGE_PATH}
+          element={<ProfileAccountSettingManagePage />}
+        />
+        <Route
+          path={PROFILE_EMAIL_EDIT_PATH}
+          element={<ProfileAccountEmailEditPage />}
+        />
+        <Route
+          path={PROFILE_BIRTHDATE_EDIT_PATH}
+          element={<ProfileAccountBirthdateEditPage />}
+        />
+        <Route
+          path={PROFILE_GENDER_EDIT_PATH}
+          element={<ProfileAccountGenderEditPage />}
+        />
+        <Route
+          path={PROFILE_PASSWORD_EDIT_PATH}
+          element={<ProfileAccountPasswordEditPage />}
+        />
+        <Route
+          path={PROFILE_DELETE_ACCOUNT_PATH}
+          element={<ProfileAccountDeleteAccountPage />}
+        />
+        <Route
+          path={PROFILE_PRIVACY_POLICY_PATH}
+          element={<ProfileAccountSettingPrivacyPage />}
+        />
+        <Route
+          path={PROFILE_PRIVATE_PROFILE_PATH}
+          element={<ProfileAccountPrivateProfilePage />}
+        />
+        <Route
+          path={PROFILE_BLOCKED_ACCOUNT_PATH}
+          element={<ProfileBlockedUserListManagePage />}
         />
         <Route path={POST_COMPOSE_PATH} element={<PostComposePage />} />
         <Route
           path={SEARCH_FAVORITE_LIST_PATH}
           element={<SearchFavoriteTermEditPage />}
         />
+        <Route path={LOGIN_PATH} element={<LoginPage />} />
+        <Route path={SIGNUP_PATH} element={<SignupPage />} />
+
+        <Route
+          path={SEARCH_FAVORITE_LIST_PATH}
+          element={<SearchFavoriteTermEditPage />}
+        />
+
+        <Route path={NOTIFICATION_LIST_PATH} element={<NotificatinoPage />} />
       </Routes>
     </BrowserRouter>
   );
