ddfc8af61ca7e659e3f2010e69643090969e51a4
Changmin Kim <ckd29672@naver.com>
2025-04-21 16:38:18 +0900

    fix: GA4: 프로덕트 환경에서만 적용되도록

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,24 @@ if (container.hasChildNodes()) {
   root.render(<App />);
 }
 
+const GA_ID = process.env.REACT_APP_GA_ID;
+
+if (process.env.NODE_ENV === 'production' && GA_ID) {
+  const script1 = document.createElement('script');
+  script1.async = true;
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement('script');
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_ID}');
+  `;
+  document.head.appendChild(script2);
+}
+
 // Service Worker 등록
 serviceWorkerRegistration.register();
 
